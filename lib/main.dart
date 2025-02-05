813462891ee41f543837c5950a4815ef4c8f83fd
anhnguyen6535 <anhnguyen6535@gmail.com>
2025-02-05 15:16:04 -0700

    create project

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        colorScheme: ColorScheme(
+          brightness: Brightness.light, 
+          primary: Colors.yellow[700]!, 
+          onPrimary: Colors.black, 
+          secondary: Colors.amber[600]!, 
+          onSecondary: Colors.black, 
+          error: Colors.red[700]!, 
+          onError: Colors.white, 
+          surface: Color(0xFFFEF293), 
+          onSurface: Colors.black,
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.yellow,
+          foregroundColor: Colors.black,
+        )
+      ),
+    );
+  }
+}
+
