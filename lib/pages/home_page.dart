fb04d0b5fd2c3361ea34e9aa36ee6d47f1b43a9a
anhnguyen6535 <anhnguyen6535@gmail.com>
2025-02-05 16:52:34 -0700

    add functionality to create new task

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/utils/dialog_box.dart';
 import 'package:todoapp/utils/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
   List todoList = [
     ["Make tut", false],
     ["Do exercise", false],
@@ -21,6 +24,28 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void saveNewTask(){
+    print(_controller.text);
+    setState(() {
+      todoList.add([_controller.text, false]);
+    });
+    _controller.clear();
+    Navigator.of(context).pop();
+  }
+
+  void createNewTask(){
+    showDialog(
+      context: context, 
+      builder: (context){
+        return DialogBox(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),  
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +53,13 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("TO DO"),
         elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
       ),
       body: ListView.builder(
         itemCount: todoList.length,
