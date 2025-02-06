5d83469b2c6fd9328e75b7d726a319545ae01c1a
anhnguyen6535 <anhnguyen6535@gmail.com>
2025-02-06 13:51:40 -0700

    update db functions

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -1,23 +1,36 @@
 import 'package:hive_flutter/hive_flutter.dart';
 
-class TodoDatabase{
+class AppDatabase{
   List todoList = [];
+  List choresList = [];
   final _mybox = Hive.box('mybox');
-  
-  // only run the first time openning this app
-  void createInitialPersonalTasks(){
+
+  // this function only runs the first time openning this app
+  void createInitialData(){
     todoList = [
       ["Make tut", false],
       ["Do exercise", false],
       ["Bake", false],
     ];
+    choresList = [
+      ["Do laundry", false],
+      ["Buy groceries", false],
+    ];
+  }
+  
+  void loadData(String listName){
+    if(listName == "personal"){
+      todoList = _mybox.get("TODOLIST");  
+    }else if(listName == "chores"){
+      choresList = _mybox.get("CHORESLIST");  
+    }
   }
 
-  void loadPersonalTasksData(){
-    todoList = _mybox.get("TODOLIST");
-  }
-
-  void updatePersonalTasksDb(){
-    _mybox.put("TODOLIST", todoList);
+  void updateDb(String listName){
+    if(listName == "personal"){
+      _mybox.put("TODOLIST", todoList);  
+    }else if(listName == "chores"){
+      _mybox.put("CHORESLIST", choresList);  
+    }
   }
 }
